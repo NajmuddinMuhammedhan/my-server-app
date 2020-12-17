84e1a4d7226a6af7e55548010f0ed584c1843a92
Najmuddin Muhammedhan <najmuddin.muhammedhan@gmail.com>
2020-12-17 15:24:21 -0800

    Changes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const PORT = process.env.PORT || 4000
 
 const app = express()
+
+app.use(cors())
 
 app.get('/', (req, res) => {
 	res.send('Home Page')
